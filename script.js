b6f1bfb1d4484a64fa490d44eb78aaa214ebc211
Ilgiz Temi <temirbekov91@gmail.com>
2022-02-28 23:54:12 -0500

    fortune-teller-was-completed

---
+++ script.js
@@ -0,0 +1,5 @@
+let children = 'N';
+let partner = 'Z';
+let geoLocation = 'Y';
+let jobTitle = 'X';
+console.log("You will be a " + jobTitle + " in " + geoLocation + ", and married to " + partner + " with " + children + " kids.");
